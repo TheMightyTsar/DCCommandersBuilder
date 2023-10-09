8de6ef945d341ec27b8b113e5f96496cf70324a3
Felipe Vidal Fuentes <fvidalf@uc.cl>
2023-10-09 17:37:01 -0300

    Lista preparación de tests de estructura (falta completar con atributos y métodos a testear)

--- src/review/reviewHandler.py
+++ src/review/reviewHandler.py
@@ -3,26 +3,61 @@ from src.review.utils.globals_modder import (
     add_globals_to_file,
     delete_extra_lines
 )
+from src.review.utils.print_styles import (
+    print_test_pass,
+    print_test_fail
+)
 import sys
 from src.review.reviewExceptions import ModuloNoPermitido
 
 
 def check_code(commanderName):
     # debe retornar True si el codigo es Valido
+    print(f"Validando commander.py de {commanderName}... ")
     try:
-        check_imported_modules(commanderName)
+        print("- Test sintaxis...", end="")
+        user_module = importlib.import_module(
+            f"created_commanders.{commanderName}.commander"
+        )
+        print_test_pass()
+
+        print("- Test modulos importados...", end="")
+        check_imported_modules(commanderName, user_module)
+        print_test_pass()
+
+        print("- Test estructura...", end="")
+        check_commander_structure(user_module)
+        print_test_pass()
 
     except ModuloNoPermitido as error:
+        print_test_fail()
         print("Código commander.py inválido")
         print(f"Motivo: {error.args[0]}")
         error.imprimir_modulos_invalidos()
+        return False
+
+    except (NameError, AttributeError, TypeError) as error:
+        print_test_fail()
+        print("Código commander.py inválido")
+        print(f"Motivo: {error.args[0]}")
+        return False
+
+    except SyntaxError as error:
+        print_test_fail()
+        print("Código commander.py inválido")
+        print("Motivo: Error de sintaxis")
+        print("----")
+        print(f"Línea: {error.lineno}\n")
+        print(error.text, end="")
+        print((error.offset - 1) * " " + "^\n")
+        print("SyntaxError:", error.msg)
         return False
 
     return True
 
 
 # Definir tester de modulos importados
-def check_imported_modules(commanderName):
+def check_imported_modules(commanderName, user_module):
     '''
     Revisa si los módulos importadoes en el código del usuario son válidos.
     En caso de encontrar módulos inválidos, levanta una excepción con los
@@ -30,15 +65,18 @@ def check_imported_modules(commanderName):
     En caso contrario, no retorna nada.
     '''
 
-    modules_whitelist = list(sys.modules).copy()
-    modules_whitelist.extend(["math", "random", "time"])
+    modules_whitelist = [
+        "itertools", "importlib", "sys", "math", "random", "time"
+    ]
 
     # Modificar archivo commander.py para agregar función get_globals
     original_lines = add_globals_to_file(commanderName)
 
-    # commanderName -> Referencia a un directorio -> En su interior commander
-    # Importar archivo creado por usuario con commanderName
-    user_module = importlib.import_module(f"created_commanders.{commanderName}.commander")
+    # Re-importar modulo usuario
+    user_module = importlib.import_module(
+        f"created_commanders.{commanderName}.commander"
+    )
+    user_module = importlib.reload(user_module)
     modules_whitelist.append(f"created_commanders.{commanderName}.commander")
     modules_whitelist.append(f"created_commanders.{commanderName}")
     modules_whitelist.append("created_commanders")
@@ -65,6 +103,39 @@ def check_imported_modules(commanderName):
     if invalid_modules:
         raise ModuloNoPermitido(
             invalid_modules, "Se han encontrado módulos no permitidos:")
+
+
+def check_commander_structure(user_module):
+    '''
+    Revisa si el código para la clase Commander es válido o no.
+    Debe poder instanciarse con los argumentos requeridos, y deben existir
+    los métodos necesarios.
+    '''
+
+    # Importar archivo creado por usuario con commanderName
+
+    try:
+        commander = user_module.Commander()
+    except (NameError, AttributeError):
+        raise NameError(
+            "No se ha encontrado la clase Commander en commander.py.")
+    except TypeError:
+        raise TypeError(
+            "La clase Commander no debe recibir argumentos.")
+
+    # Revisar si la clase Commander tiene los atributos necesarios
+    try:
+        commander.atr1
+    except AttributeError:
+        raise AttributeError(
+            "No se ha encontrado el atributo atr1 en la clase Commander.")
+
+    # Revisar si la clase Commander tiene los métodos necesarios
+    try:
+        commander.metodo()
+    except AttributeError:
+        raise AttributeError(
+            "No se ha encontrado el método metodo en la clase Commander.")
 
 
 def merge(dict1: dict, dict2: dict):
