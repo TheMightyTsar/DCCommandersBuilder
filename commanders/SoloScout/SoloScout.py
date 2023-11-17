8ae8ce7de106ea3a8700051c89b40ebef7494f4c
DAMIAN GUBBINS <damiangubbins@uc.cl>
2023-11-17 13:13:07 -0300

    base bot struct

---
+++ commanders/SoloScout/SoloScout.py
@@ -0,0 +1,37 @@
+"""Commander file for SoloScout."""
+
+
+from src.base_files.base_classes import BaseCommander
+from src.base_files.parametros import (ATACAR, GAUSS, HIMARS, MOVER, SCOUT, SOLDIER, TOWER)
+
+
+class Commander(BaseCommander):
+    """Commander for SoloScout."""
+
+    def __init__(self):
+        super().__init__(nombre="SoloScout")
+        # Define aquí atributos adicionales para tu comandante
+
+    def montar_tablero(self):
+        # Define aquí las posciciones iniciales de tus tropas
+        tropas = {
+            SOLDIER: ["A0", "A1", "A2", "A3", "A4"],
+
+            HIMARS: ["B0", "B1"],
+
+            SCOUT: ["C0", "C1"],
+
+            GAUSS: ["D0", "D1"],
+
+            TOWER: ["E0"],
+        }
+
+        my_troops, troop_list = self.instanciar_tropas(tropas)
+
+        self.tropas = my_troops
+
+        return troop_list
+
+    def jugar_turno(self, reporte, reporte_enemigo):
+        # Completa tu código aquí
+        ...
